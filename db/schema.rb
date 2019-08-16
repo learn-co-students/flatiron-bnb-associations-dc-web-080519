23a99b78488e12e363f7e568efba0cdf488cb6b1
Pete Hanner <petehanner@gmail.com>
2019-08-16 14:40:56 -0400

    database structure with all necessary tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190816183727) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string  "title"
+    t.string  "description"
+    t.string  "address"
+    t.string  "listing_type"
+    t.integer "price"
+    t.integer "neighborhood_id"
+    t.integer "host_id"
+  end
+
+  create_table "neighborhoods", force: :cascade do |t|
+    t.string  "name"
+    t.integer "city_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string  "checkin"
+    t.string  "checkout"
+    t.integer "listing_id"
+    t.integer "guest_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string  "description"
+    t.integer "rating"
+    t.integer "guest_id"
+    t.integer "reservation_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
