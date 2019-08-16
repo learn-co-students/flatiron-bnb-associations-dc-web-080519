59246a425ee99dc7a8eec266f0deed40334216c6
Pete Hanner <petehanner@gmail.com>
2019-08-16 16:30:47 -0400

    cleaned up (mostly)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
+    # host functionality
     has_many :listings, :foreign_key => 'host_id'
 
+    has_many :reservations, through: :listings
+
+    # guest functionality
+
+    # user#trips is alias for user#reservations
     has_many :trips, :class_name => "Reservation", :foreign_key => 'guest_id'
 
     has_many :reviews, :foreign_key => 'guest_id'
-    
-    has_many :reservations, through: :listings, :class_name => "Reservation"
 end
